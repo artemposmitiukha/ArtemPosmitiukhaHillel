43ea9fca4b3c04388ad9e4eee1e742663decf917
macbook <artemposmitiukha@gmail.com>
2024-04-10 18:16:27 +0300

    Homework_2_5

---
+++ Homework_2/Homework_2_5/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Homework_2_5
+{
+    class Program
+    {
+        // Користувач вводить радіус круга. Напишіть програму, яка обчислює площу цього круга і виводить в консоль результат. 
+        
+        static void Main(string[] args)
+        {
+            Console.Write("Enter circle radius: ");
+            double square = Math.Round(Math.PI * Math.Pow(int.Parse(Console.ReadLine()), 2), 1);
+            Console.WriteLine("Circle area: " + square);
+        }
+    }
+}
