b2abd0187ae9c53c63790c55f6fc1c5ccc4543a8
macbook <artemposmitiukha@gmail.com>
2024-04-10 18:26:03 +0300

    Homework_2_2

---
+++ Homework_2/Homework_2_2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Homework_2_2
+{
+    class Program
+    {
+        // Написати програму, яка обчислює квадрат будь-якого введеного числа (бажано використати Math).
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number to square: ");
+            double num = Math.Pow(double.Parse(Console.ReadLine()), 2);
+            // int num = (int)Math.Pow(double.Parse(Console.ReadLine()), 2);
+
+            Console.WriteLine("The square of your number: " + num);
+
+        }
+    }
+}
