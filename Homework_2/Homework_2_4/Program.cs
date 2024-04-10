9c09b645e8826ea0e63b4b0e620de26e6dbd4a6e
macbook <artemposmitiukha@gmail.com>
2024-04-10 18:16:10 +0300

    Homework_2_4

--- Homework_2/Homework_2_4/Program.cs
+++ Homework_2/Homework_2_4/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace Homework_2_4;
-
-class Program
+﻿namespace Homework_2_4
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        // Написати програму, яка обчислює середнє арифметичне двох чисел.
+        
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int result = (num1 + num2) / 2;
+            Console.WriteLine("Arithmetic mean: " + result);
+        }
     }
 }
