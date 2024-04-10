947305600622fcaedd31bceaa3af4d8f4f244627
macbook <artemposmitiukha@gmail.com>
2024-04-10 16:58:13 +0300

    Homework_2_1

--- Homework_2/Homework_2_1/Program.cs
+++ Homework_2/Homework_2_1/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace Homework_2;
-
-class Program
+﻿namespace Homework_2
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        // Дано тризначне число. Знайти число, отримане під час прочитання його цифр справа наліво.
+        static void Main(string[] args)
+        {
+            int initialNum = int.Parse(Console.ReadLine());
+            int resultNum = (initialNum % 10) * 100 + (initialNum / 10 % 10) * 10 + initialNum / 100;
+            Console.WriteLine(resultNum);
+        }
     }
 }
