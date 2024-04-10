205bcdd420e5e92126a584790e0257415b169978
macbook <artemposmitiukha@gmail.com>
2024-04-10 17:37:58 +0300

    Homework_2_3

---
+++ Homework_2/Homework_2_3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Homework_2_3;
+
+class Program
+{
+    // Дано тризначне число. Знайти число, отримане під час перестановки першої та другої цифр заданого числа.
+         
+    static void Main(string[] args)
+    {
+        int initialNum = 936;
+        Console.WriteLine("Initial number: " + initialNum);
+            
+        int reversedNum = (initialNum % 10 * 100) + (initialNum / 10 % 10) * 10 + (initialNum / 100);
+        Console.WriteLine("Reversed number: " + reversedNum);
+    }
+}
