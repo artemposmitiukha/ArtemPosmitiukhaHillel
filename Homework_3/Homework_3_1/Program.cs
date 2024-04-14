34181d612cc45b19b8a8cb8cf550ded17acce562
macbook <artemposmitiukha@gmail.com>
2024-04-14 11:02:50 +0300

    Homework_3_1

---
+++ Homework_3/Homework_3_1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Homework_3_1
+{
+    // Необхідно написати консольну програму, де користувач вводитиме з клавіатури 2 числа. Числа будуть порівнюватися з наступним 
+    // виведенням в консоль результату цього порівняння (чи рівні значення, а якщо ні, яке число більше/менше).
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number 1: ");
+            double num1 = double.Parse(Console.ReadLine());
+            
+            Console.Write("Enter number 2: ");
+            double num2 = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine(num1 == num2 ? "Numbers are equal" : 
+                $"Number {(num1 > num2 ? "1" : "2")} is bigger than number {(num1 > num2 ? "2" : "1")}");
+        }
+    }
+}
