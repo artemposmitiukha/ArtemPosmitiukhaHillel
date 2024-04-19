74f29e72c54b943b4615478e14689f1efa2a8d7d
macbook <artemposmitiukha@gmail.com>
2024-04-19 21:58:49 +0300

    Homework_4_2

---
+++ Homework_4/Homework_4_2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Homework_4_2
+{
+    class Program
+    {
+        
+        // Програма загадує число від 1 до 146 (привіт, Random). Користувач намагається вгадати його.
+        // У разі неправильної відповіді програма підказує «більше» або «менше»
+        
+        static void Main(string[] args)
+        {
+            byte randomNum = (byte) new Random().Next(1, 146), playerNum;
+            do
+            {
+                Console.Write("Guess the number from 1 to 146: ");
+                playerNum = byte.Parse(Console.ReadLine());
+                Console.WriteLine(playerNum == randomNum ? "You win!" : 
+                                  playerNum > randomNum ? "Your num is bigger" : "Your num is smaller");
+            } while (randomNum != playerNum);
+        }
+    }
+}
