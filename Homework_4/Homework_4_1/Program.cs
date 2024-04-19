e41af25f998e15a81eadb82a68da741edb6d6700
macbook <artemposmitiukha@gmail.com>
2024-04-19 21:58:41 +0300

    Homework_4_1

---
+++ Homework_4/Homework_4_1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Homework_4_1
+{
+    class Program
+    {
+        
+        // Напишіть програму, яка «запитуватиме» правильний пароль, доки він не буде введений.
+        // Правильний пароль нехай буде "root". Якщо пароль неправильний, програма повинна сказати "Неправильний пароль!"
+        
+        static void Main(string[] args)
+        {
+            string password;
+            do
+            {
+                Console.Write("Enter your password: ");
+                password = Console.ReadLine();
+                Console.WriteLine(password == "root" ? "Welcome!" : "Wrong password!");
+            } while (password != "root");
+        }
+    }
+}
