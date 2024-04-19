0e9fe506f3a53d80ebf6dee0450b9509502df6a7
macbook <artemposmitiukha@gmail.com>
2024-04-19 21:58:59 +0300

    Homework_4_3

---
+++ Homework_4/Homework_4_3/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Homework_4_3
+{
+    class Program
+    {
+        
+        // Доробити таблицю множення - зробити гарний вивід в консоль.  
+        
+        static void Main(string[] args)
+        {
+            /*for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    i = 1;
+                    while (i <= 10)
+                    {
+                        Console.Write($"{i} * {j} = {i * j}\t\t");
+                        i++;
+                    }
+                    Console.WriteLine();
+                }
+            }*/
+            for (int j = 1; j <= 10; j++)
+            {
+                int i = 1;
+                while (i <= 10)
+                {
+                    Console.Write($"{i} * {j} = {i * j}\t\t");
+                    i++;
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
