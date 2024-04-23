909c755801e7648ebeed2fa660aab5ab4b4ca324
macbook <artemposmitiukha@gmail.com>
2024-04-23 22:05:08 +0300

    Homework_5_4

---
+++ Homework_5/Homework_5_4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using Microsoft.VisualBasic;
+
+namespace Homework_5_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] myArray = new int[random.Next(6, 12)];
+            for (int i = 0; i < myArray.Length; i++)
+                myArray[i] = random.Next(-5, 5);
+            Console.WriteLine("\nInitial array: " + string.Join(" | ", myArray));
+            Console.Write("Array till -1: ");
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if(myArray[i] != -1)
+                    Console.Write(" | " + myArray[i]);
+                else
+                    break;
+            }
+            
+        }
+    }
+}
