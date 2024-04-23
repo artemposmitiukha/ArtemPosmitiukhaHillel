d033903466ad3f8ee613b2adc5969642c2de553c
macbook <artemposmitiukha@gmail.com>
2024-04-23 22:04:43 +0300

    Homework_5_2

---
+++ Homework_5/Homework_5_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.AccessControl;
+
+namespace Homework_5_2
+{
+    class Program
+    {
+        
+        // Написати програму для інверсії масиву, тобто перевороту його у зворотному порядку.
+        // В цьому завданні недостатньо просто вивести елементи масиву у зворотному порядку. 
+
+
+        
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Console.Write("Enter array length: ");
+            int[] myArray = new int[int.Parse(Console.ReadLine())], reversedArray = new int[myArray.Length];
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                myArray[i] = random.Next(-100, 100);
+                reversedArray[myArray.Length - i - 1] = myArray[i];
+            }
+            
+            Console.WriteLine("Initial array: " + string.Join(" | ", myArray) + 
+                              "\nReversed array: " + string.Join(" | ", reversedArray));
+            /*
+            Array.Reverse(myArray);
+            Console.WriteLine("Reversed array: " + string.Join(" | ", myArray));
+            */
+
+        }
+    }
+}
