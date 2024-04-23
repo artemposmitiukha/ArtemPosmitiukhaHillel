5571113f769a509efd7986837558dae8d26b965e
macbook <artemposmitiukha@gmail.com>
2024-04-23 22:04:35 +0300

    Homework_5_1

---
+++ Homework_5/Homework_5_1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Homework_5_1
+{
+    class Program
+    {
+        
+        // Задати масив із 10 елементів. Заповнити цей масив рандомними числами від -100 до 100.
+        // Знайти кількість додатніх чисел у масиві. Вивести на екран масив і кількість порахованих чисел. 
+        
+        static void Main(string[] args)
+        {
+            byte numCount = 0;
+            Random random = new Random();
+            sbyte[] myArray = new sbyte [10];
+            Console.Write("Your array: ");
+            for (int i = 0;i < myArray.Length; i++)
+            {
+                myArray[i] = (sbyte)random.Next(-100, 100);
+                Console.Write(myArray[i] + ", ");
+                if (myArray[i] > 0)
+                    numCount++;
+            }
+            Console.WriteLine("\nCount of positive numbers: " + numCount);
+        }
+    }
+}
