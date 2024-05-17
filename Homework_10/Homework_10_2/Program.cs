0a910b6d98b13af82684348000c5026f6cd74c63
macbook <artemposmitiukha@gmail.com>
2024-05-17 23:03:58 +0300

    Homework_10_2

---
+++ Homework_10/Homework_10_2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace  Homework_10_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double age;
+        while (true)
+        {
+            try
+            {
+                Console.Write("Enter your age: ");
+                age = double.Parse(Console.ReadLine());
+                if (age > 0 && (age % 1 == 0))
+                {
+                    Console.WriteLine("Your age: " + age);
+                    break;
+                }
+                else
+                    throw new FormatException("Invalid age!");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Your age in the wrong format!");
+            }
+        }
+    }
+}
