5ab7bf0fd0218ee21140e4e804bc080b8c4f5cbb
macbook <artemposmitiukha@gmail.com>
2024-05-17 23:03:21 +0300

    Homework_10_1

---
+++ Homework_10/Homework_10_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using UkrainianStringUtils;
+namespace Homework_10_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string userInput = Console.ReadLine();
+        
+        int vowelsCount = UkrainianStringUtils.UkrainianStringUtils.VowelsCount(userInput);
+        Console.WriteLine("Number of vowels: " + vowelsCount);
+        
+        int consonantsCount = UkrainianStringUtils.UkrainianStringUtils.CountConsonants(userInput);
+        Console.WriteLine("Number of consonants: " + consonantsCount);
+        
+        string reversedString = UkrainianStringUtils.UkrainianStringUtils.ReverseString(userInput);
+        Console.WriteLine("reversed string: " + reversedString);
+        
+        string withoutDuplicates = UkrainianStringUtils.UkrainianStringUtils.RemoveDuplicates(userInput);
+        Console.WriteLine("String without duplicates: " + withoutDuplicates);
+        
+        string withoutPunctuation = UkrainianStringUtils.UkrainianStringUtils.RemovePunctuation(userInput);
+        Console.WriteLine("String without punctuation: " + withoutPunctuation);
+    }
+}   
